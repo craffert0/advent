bd57386dda9196bb04e400a7415835bc3199b3f3
Colin Rafferty <colin@rafferty.net>
2024-12-07 09:39:40 -0500

    2024/07/one

---
+++ 2024/07/one.py
@@ -0,0 +1,17 @@
+# -*- compile-command: "python3 one.py < sample.txt" -*-
+
+import sys
+
+def guess(result, partial, rest):
+    if len(rest) == 0:
+        return result == partial
+    return guess(result, partial + rest[0], rest[1:]) or guess(result, partial * rest[0], rest[1:])
+
+sum = 0
+for line in sys.stdin:
+    first = line.split(':')
+    result = int(first[0])
+    operands = [int(x) for x in first[1].split()]
+    if guess(result, operands[0], operands[1:]):
+        sum = sum + result
+print(sum)
